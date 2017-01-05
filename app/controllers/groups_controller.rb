4cbaa7e878111c0412051cfccba76bff1fbbc8f9
mali <mailfullstack@gmail.com>
2017-01-05 12:35:05 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,13 +13,18 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
+
+
   end
 
   def create
     @group = Group.new(group_params)
-    @group.save
 
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def update
